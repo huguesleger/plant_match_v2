24c512d85421456f79c396831c5a896d56867560
hugues leger <hugueslegerdevweb@gmail.com>
2025-02-04 20:28:24 +0100

    refacto profilScreen

--- lib/presentation/profil/presentation/profil_screen.dart
+++ lib/presentation/profil/presentation/profil_screen.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plant_match_v2/core/theme/app_colors.dart';
-import 'package:plant_match_v2/core/widgets/error/error_page.dart';
-import 'package:plant_match_v2/presentation/profil/data/firebase_profil_repo.dart';
 import 'package:plant_match_v2/presentation/profil/domain/entity/profil_user.dart';
-import 'package:plant_match_v2/presentation/profil/presentation/cubit/profil_cubit.dart';
-import 'package:plant_match_v2/presentation/profil/presentation/cubit/profil_state.dart';
 import 'package:plant_match_v2/presentation/profil/presentation/profil_card/profil_card.dart';
 import 'package:plant_match_v2/presentation/profil/presentation/profil_header/profil_header.dart';
 import 'package:plant_match_v2/presentation/profil/presentation/profil_navigation/profil_navigation.dart';
-import 'package:plant_match_v2/presentation/storage/data/firebase_storage_repository.dart';
 
 class ProfilScreen extends StatelessWidget {
-  ProfilScreen({
+  const ProfilScreen({
     super.key,
     required this.profilUser,
     required this.userId,
@@ -20,79 +14,46 @@ class ProfilScreen extends StatelessWidget {
 
   final ProfilUser profilUser;
   final String userId;
-  final profilRepository = FirebaseProfilRepo();
-  final storageRepository = FirebaseStorageRepository();
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ProfilCubit(
-          profilRepository: profilRepository,
-          storageRepository: storageRepository)
-        ..getProfilUser(userId),
-      child: Scaffold(
-        body: SafeArea(
-          child: BlocBuilder<ProfilCubit, ProfilState>(
-            builder: (context, state) {
-              return switch (state) {
-                ProfilInitial() || ProfilLoading() => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                ProfilError() => ErrorPage(errorMessage: state.message),
-                ProfilLoaded() =>
-                  _ContentScreen(profilUser: profilUser, userId: userId),
-              };
-            },
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ProfilHeader(profilUser: profilUser),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              height: 216,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ProfilCard(userId: userId),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              child: DecoratedBox(
+                decoration: const BoxDecoration(
+                  color: AppColors.greyUltraLight,
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  child: ProfilNavigation(profilUser: profilUser),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-}
-
-class _ContentScreen extends StatelessWidget {
-  const _ContentScreen({
-    required this.profilUser,
-    required this.userId,
-  });
-
-  final ProfilUser profilUser;
-  final String userId;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: ProfilHeader(profilUser: profilUser),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        SizedBox(
-          height: 216,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ProfilCard(userId: userId),
-          ),
-        ),
-        const SizedBox(
-          height: 25,
-        ),
-        Expanded(
-          child: DecoratedBox(
-            decoration: const BoxDecoration(
-              color: AppColors.greyUltraLight,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-              child: ProfilNavigation(profilUser: profilUser),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
