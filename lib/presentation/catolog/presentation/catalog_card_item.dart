96708875be8dad8432068a127fafa7130c4e7a61
hugues leger <hugueslegerdevweb@gmail.com>
2025-06-26 15:15:00 +0200

    style card catalog item

--- lib/presentation/catolog/presentation/catalog_card_item.dart
+++ lib/presentation/catolog/presentation/catalog_card_item.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:plant_match_v2/core/theme/app_colors.dart';
+import 'package:plant_match_v2/core/widgets/badge/badge_pill.dart';
 import 'package:plant_match_v2/presentation/catolog/domain/entity/catalog.dart';
+import 'package:plant_match_v2/presentation/catolog/presentation/util/environment_name.dart';
+import 'package:plant_match_v2/presentation/catolog/presentation/util/family_name.dart';
 
 class CatalogCardItem extends StatelessWidget {
   const CatalogCardItem({super.key, required this.catalog});
@@ -10,64 +14,119 @@ class CatalogCardItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: AppColors.white,
-          boxShadow: [
-            BoxShadow(
-              color: AppColors.black.withOpacity(0.1),
-              blurRadius: 4,
-              offset: const Offset(0, 2),
-            ),
-          ],
-        ),
-        padding: const EdgeInsets.all(12),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: catalog.images.isNotEmpty &&
-                      catalog.images.first.startsWith('http')
-                  ? Image.network(
-                      catalog.images.first,
-                      height: 90,
-                      width: 100,
-                      fit: BoxFit.cover,
-                    )
-                  : const Image(
-                      image: AssetImage('assets/images/empty_picture.png'),
-                      height: 90,
-                      width: 100,
-                      fit: BoxFit.cover,
+      padding: const EdgeInsets.only(top: 15),
+      child: Material(
+        elevation: 6,
+        shadowColor: AppColors.black.withValues(alpha: 0.2),
+        borderRadius: BorderRadius.circular(12),
+        clipBehavior: Clip.antiAlias,
+        color: AppColors.white,
+        child: InkWell(
+            onTap: () {},
+            child: SizedBox(
+              height: 120,
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: catalog.images.isNotEmpty &&
+                              catalog.images.first.startsWith('http')
+                          ? Image.network(
+                              catalog.images.first,
+                              height: 100,
+                              width: 80,
+                              fit: BoxFit.cover,
+                            )
+                          : const Image(
+                              image:
+                                  AssetImage('assets/images/empty_picture.png'),
+                              height: 100,
+                              width: 80,
+                              fit: BoxFit.cover,
+                            ),
                     ),
-            ),
-            const SizedBox(width: 12),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    catalog.name,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      catalog.name,
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  const Icon(
+                                    LucideIcons.bookmark_x,
+                                    color: AppColors.greyMedium,
+                                    size: 22,
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 6),
+                              Text(
+                                catalog.description,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    fontSize: 12, color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: [
+                                BadgePill(
+                                  text: Text(
+                                    catalog.environment.envName,
+                                    style: const TextStyle(
+                                        fontSize: 12,
+                                        color: AppColors.greenDark),
+                                  ),
+                                  badgeColor: AppColors.greenLight,
+                                ),
+                                const SizedBox(width: 8),
+                                Wrap(
+                                  spacing: 8,
+                                  runSpacing: 4,
+                                  children: catalog.family.map((f) {
+                                    return BadgePill(
+                                      text: Text(
+                                        f.familyName,
+                                        style: const TextStyle(
+                                            fontSize: 12,
+                                            color: AppColors.greenDark),
+                                      ),
+                                      badgeColor: AppColors.greenLight,
+                                    );
+                                  }).toList(),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 6),
-                  Text(
-                    catalog.description,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
+            )),
       ),
     );
   }
