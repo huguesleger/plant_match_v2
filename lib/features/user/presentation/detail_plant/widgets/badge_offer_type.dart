11d832662d0db91db57078f5a07d9179b617978b
hugues leger <hugueslegerdevweb@gmail.com>
2025-12-22 16:43:25 +0100

    add parameter fontSize

--- lib/features/user/presentation/detail_plant/widgets/badge_offer_type.dart
+++ lib/features/user/presentation/detail_plant/widgets/badge_offer_type.dart
@@ -5,9 +5,14 @@ import 'package:plant_match_v2/core/widgets/badge/badge_pill.dart';
 import 'package:plant_match_v2/features/catolog/domain/entity/catalog.dart';
 
 class BadgeOfferType extends StatelessWidget {
-  const BadgeOfferType({super.key, required this.offerType});
+  const BadgeOfferType({
+    super.key,
+    required this.offerType,
+    this.fontSize = AppTypo.textXs,
+  });
 
   final OfferType offerType;
+  final double fontSize;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +20,7 @@ class BadgeOfferType extends StatelessWidget {
       text: Text(
         offerType.offerTypeName,
         style: TextStyle(
-          fontSize: AppTypo.textXs,
+          fontSize: fontSize,
           color: offerType == OfferType.donation
               ? AppColors.white
               : AppColors.greenLight,
